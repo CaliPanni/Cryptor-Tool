b47e6141be31d5a99cbba7db7f70d9500ee16bee
CaliPanni <97522907+CaliPanni@users.noreply.github.com>
2022-12-20 13:53:30 +0100

    v 1.0

---
+++ cripter/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(14, 272)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 76)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Load .cl0 or .txt"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(119, 272)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 76)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Decript Text"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(14, 15)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(303, 211)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(230, 272)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(87, 76)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Export .cl0 or .txt"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("CRYSTAL", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(14, 236)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(303, 28)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Cript text!"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.FileName = "crypted_file"
+        Me.SaveFileDialog1.Filter = "FILE CL0|*.cl0|FILE TXT|*.txt|All File|*.*"
+        Me.SaveFileDialog1.Title = "Export Crypted/Decrypted file"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "FILE CL0|*.cl0|FILE TXT|*.txt|All File|*.*"
+        Me.OpenFileDialog1.Title = "Load Crypted/Decrypted File"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(331, 356)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Calipanni's cl0 Cryptor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ cripter/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text.Replace("!", "a")
+        TextBox1.Text = TextBox1.Text.Replace("|", "b")
+        TextBox1.Text = TextBox1.Text.Replace("£", "c")
+        TextBox1.Text = TextBox1.Text.Replace("$", "d")
+        TextBox1.Text = TextBox1.Text.Replace("%", "e")
+        TextBox1.Text = TextBox1.Text.Replace("&", "f")
+        TextBox1.Text = TextBox1.Text.Replace("/", "g")
+        TextBox1.Text = TextBox1.Text.Replace("(", "h")
+        TextBox1.Text = TextBox1.Text.Replace(")", "i")
+        TextBox1.Text = TextBox1.Text.Replace("=", "l")
+        TextBox1.Text = TextBox1.Text.Replace("?", "m")
+        TextBox1.Text = TextBox1.Text.Replace("ì", "n")
+        TextBox1.Text = TextBox1.Text.Replace("^", "o")
+        TextBox1.Text = TextBox1.Text.Replace("'", "p")
+        TextBox1.Text = TextBox1.Text.Replace("+", "q")
+        TextBox1.Text = TextBox1.Text.Replace("§", "r")
+        TextBox1.Text = TextBox1.Text.Replace("-", "s")
+        TextBox1.Text = TextBox1.Text.Replace(":", "t")
+        TextBox1.Text = TextBox1.Text.Replace("ç", "u")
+        TextBox1.Text = TextBox1.Text.Replace("@", "v")
+        TextBox1.Text = TextBox1.Text.Replace("#", "z")
+        TextBox1.Text = TextBox1.Text.Replace("ò", "w")
+        TextBox1.Text = TextBox1.Text.Replace("°", "j")
+        TextBox1.Text = TextBox1.Text.Replace("_", "k")
+        TextBox1.Text = TextBox1.Text.Replace(",", "x")
+        TextBox1.Text = TextBox1.Text.Replace(".", "y")
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            System.IO.File.WriteAllText(SaveFileDialog1.FileName, TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim testo As String
+            testo = System.IO.File.ReadAllText(OpenFileDialog1.FileName)
+            TextBox1.Text = testo
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        TextBox1.Text = TextBox1.Text.Replace("a", "!")
+        TextBox1.Text = TextBox1.Text.Replace("b", "|")
+        TextBox1.Text = TextBox1.Text.Replace("c", "£")
+        TextBox1.Text = TextBox1.Text.Replace("d", "$")
+        TextBox1.Text = TextBox1.Text.Replace("e", "%")
+        TextBox1.Text = TextBox1.Text.Replace("f", "&")
+        TextBox1.Text = TextBox1.Text.Replace("g", "/")
+        TextBox1.Text = TextBox1.Text.Replace("h", "(")
+        TextBox1.Text = TextBox1.Text.Replace("i", ")")
+        TextBox1.Text = TextBox1.Text.Replace("l", "=")
+        TextBox1.Text = TextBox1.Text.Replace("m", "?")
+        TextBox1.Text = TextBox1.Text.Replace("n", "ì")
+        TextBox1.Text = TextBox1.Text.Replace("o", "^")
+        TextBox1.Text = TextBox1.Text.Replace("p", "'")
+        TextBox1.Text = TextBox1.Text.Replace("q", "+")
+        TextBox1.Text = TextBox1.Text.Replace("r", "§")
+        TextBox1.Text = TextBox1.Text.Replace("s", "-")
+        TextBox1.Text = TextBox1.Text.Replace("t", ":")
+        TextBox1.Text = TextBox1.Text.Replace("u", "ç")
+        TextBox1.Text = TextBox1.Text.Replace("v", "@")
+        TextBox1.Text = TextBox1.Text.Replace("z", "#")
+        TextBox1.Text = TextBox1.Text.Replace("w", "ò")
+        TextBox1.Text = TextBox1.Text.Replace("j", "°")
+        TextBox1.Text = TextBox1.Text.Replace("k", "_")
+        TextBox1.Text = TextBox1.Text.Replace("x", ",")
+        TextBox1.Text = TextBox1.Text.Replace("y", ".")
+    End Sub
+End Class
